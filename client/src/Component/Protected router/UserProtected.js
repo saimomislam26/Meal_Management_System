d78cb00e3b787e61045b25b3daebc0de7f7257ac
saimomislam26 <saimomislam16@gmail.com>
2023-03-06 21:26:09 +0600

    committed

--- client/src/Component/Protected router/UserProtected.js
+++ client/src/Component/Protected router/UserProtected.js
@@ -2,8 +2,7 @@ import React from 'react'
 import Cookies from 'js-cookie'
 import { Navigate } from 'react-router-dom'
 const UserProtected = ({children}) => {
-    const isCookie = Cookies.get('jwtoken')
-    console.log(isCookie);
+    const isCookie = Cookies.get('jwtoken',{ domain: 'meal-management-m8fx.onrender.com' })
     if(isCookie){
         return children
     }
